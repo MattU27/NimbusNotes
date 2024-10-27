62e4f5bdcb30829bc7f2388d5a7f5a2ea337426a
JM <sasukebanto@gmail.com>
2024-10-27 19:41:18 +0800

    added 1 more api key

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -3,5 +3,5 @@ import 'package:flutter/foundation.dart' show immutable;
 @immutable
 class Constants {
   static const String apiKey = '042de2606dbc19f65af076844fe94e7b';
-  static const String apiKey2 = '2d29a2d014988992c87cc2c542b847a4';
+  static const String apiKey2 = 'f198a1fed66fd4a5049f5e5e4f34bd17';
 }
